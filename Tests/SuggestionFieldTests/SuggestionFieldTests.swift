0d8d202de44eb23dda37f713ae5ac7b03b9d10f7
david-swift <106754840+david-swift@users.noreply.github.com>
2022-07-10 07:15:02 +0200

    Update SuggestionFieldTests.swift

--- Tests/SuggestionFieldTests/SuggestionFieldTests.swift
+++ Tests/SuggestionFieldTests/SuggestionFieldTests.swift
@@ -1,12 +1,16 @@
+//
+//  SuggestionFieldTests.swift
+//  SuggestionField
+//
+//  Created by david-swift
+//
+
 import SwiftUI
 import XCTest
 @testable import SuggestionField
 
 final class SuggestionFieldTests: XCTestCase {
     func testSuggestionField() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
         @State var text = ""
         let _ = SuggestionField("Hello", text: $text, divide: true, capitalized: false)
     }
